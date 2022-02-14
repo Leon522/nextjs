7075b8cee4e5edc829c5d3c82be50bb6e6882723
Leon <ming5964471402wu@qq.com>
2022-02-14 12:06:16 +0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-         asdasdasdWelcome to <a href="https://nextjs.org">Next.js!</a>
+         测试 <a href="https://nextjs.org">Next.js!</a>
         </h1>
 
         <p className={styles.description}>
